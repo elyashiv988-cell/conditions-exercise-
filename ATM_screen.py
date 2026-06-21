84c7a6f01beea7a54d2ef271cfa910b703977fc3
Elyashiv <elyashiv988@gmail.com>
2026-06-21 21:36:51 +0300

    final commit

--- ATM_screen.py
+++ ATM_screen.py
@@ -3,3 +3,15 @@ balance=500
 user_pin=int(input("Enter your PIN code"))
 if correct_pin==user_pin:
     amaount=int(input("how much money do you want to withdraw? "))
+    if amaount>balance:
+        print("not enough miney")
+    else:
+        receipt=input("do you want a rceipt? ")
+        if receipt=="yes":
+            print("withdrawal approved with receipt")
+        elif receipt=="no":
+            print("withdrawal approved without receipt")
+        else:
+            print("withdrawal approved")
+else:
+    print("Wrong PIN")

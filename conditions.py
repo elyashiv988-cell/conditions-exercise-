3b982a00707645a41c6facfb205a536770e4f39e
Elyashiv <elyashiv988@gmail.com>
2026-06-21 15:07:47 +0300

    4

--- conditions.py
+++ conditions.py
@@ -1,5 +1,29 @@
+#namber 1
 user_age=int(input("please enter your age "))
 if user_age >= 18:
     print("can enter")
 else:
     print("cannot enter")
+#namber 2
+temperature=38.2
+if temperature >37.5:
+    print("high temperature")
+else:
+    print("normal temperature")
+#namber 3
+
+stored_namber=74
+givin_namber=int(input("enter a namber"))
+if stored_namber==givin_namber:
+    print("even number")
+else:
+    print("odd number")
+# number 4
+battery=15
+is_charging=True
+if battery < 20 and is_charging==True:
+    print("low battery, charging now")
+elif battery <20 and is_charging==False:
+    print("low battery, connent charger")
+else:
+    print("battery OK")

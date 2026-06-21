144a525a510df27dd1491216d450e7648be02487
Elyashiv <elyashiv988@gmail.com>
2026-06-21 17:32:06 +0300

    added cave

--- advanture_trip.py
+++ advanture_trip.py
@@ -1,6 +1,22 @@
 trip=input("where do you want to go? ")
 match trip:
-    case forest:
+    case "forest":
         choose=input("what do you want, hide or walk ")
         if choose == "hide":
             print("you gide behind a tree")
+        elif choose == "walk":
+            print("you find a sleeping wolf")
+        else:
+            print("invalid forest action")
+    case "cave":
+        choose=input("do you have a toruh? ")
+        if choose == "yes":
+            turn= input("left or right? ")
+            if turn == "left":
+                print("you find gold")
+            elif turn == "right":
+                print("you find bats")
+            else:
+                print("invalid cave path")
+        else:
+            print("it's too dark to enter")

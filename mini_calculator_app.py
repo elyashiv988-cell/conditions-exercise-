be46a6ce258f2deda8f67fb155febbeb67206b05
Elyashiv <elyashiv988@gmail.com>
2026-06-21 18:01:41 +0300

    final commit

---
+++ mini_calculator_app.py
@@ -0,0 +1,12 @@
+nam1=float(input("enter first number"))
+nam2=float(input("enter secend number"))
+action=input("whiche action? add, subtract, multiply ")
+if action=="add":
+    print(nam1+nam2)
+elif action=="subtract":
+    print(nam1-nam2)
+elif action=="multiply":
+    print(nam1*nam2)
+else:
+    print("unknwon action")
+        
